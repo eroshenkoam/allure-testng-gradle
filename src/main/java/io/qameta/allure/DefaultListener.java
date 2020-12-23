7b07119f74fd73e0fb1d2d82cb890c9d99a6fcd0
Artem Eroshenko <eroshenkoam@me.com>
2020-12-23 17:50:06 +0300

    add group parsing

--- src/main/java/io/qameta/allure/DefaultListener.java
+++ src/main/java/io/qameta/allure/DefaultListener.java
@@ -1,11 +1,37 @@
 package io.qameta.allure;
 
-import io.qameta.allure.listener.TestLifecycleListener;
-import io.qameta.allure.model.TestResult;
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
 
-public class DefaultListener implements TestLifecycleListener {
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public void beforeTestStop(final TestResult result) {
+public class DefaultListener implements IInvokedMethodListener {
+
+    private static final Pattern LABEL_MATCHER = Pattern.compile("^(?<name>.+)[:](?<value>.+)$");
+
+    @Override
+    public void beforeInvocation(final IInvokedMethod method,
+                                 final ITestResult testResult) {
+        System.out.println("Before");
+        if (!method.isTestMethod()) {
+            return;
+        }
+        if (Objects.isNull(method.getTestMethod().getGroups())) {
+            return;
+        }
+        for (final String group : method.getTestMethod().getGroups()) {
+            final Matcher matcher = LABEL_MATCHER.matcher(group);
+            if (matcher.matches()) {
+                Allure.label(matcher.group("name"), matcher.group("value"));
+            }
+        }
+    }
+
+    @Override
+    public void afterInvocation(final IInvokedMethod method, final ITestResult testResult) {
         Allure.attachment("Before Test Stop", "Hello, World!");
     }
 
